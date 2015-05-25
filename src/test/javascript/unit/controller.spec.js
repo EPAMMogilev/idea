1d8a38fd29b2dd45ee09a2a4b10b10e947d85ae6
alxx88 <Aliaksei_Liubetski@epam.com>
2015-05-25 14:22:31 +0300

    add updateIdea controler

--- src/test/javascript/unit/controller.spec.js
+++ src/test/javascript/unit/controller.spec.js
@@ -33,11 +33,20 @@ describe('Idea controllers testing', function(){
 					'$scope':scope
 				});
 		};
+		//updateIdea
+		updateIdeaTest = function(){
+				return $controller('updateIdea', {
+					'$scope':scope
+				});
+		};
 	}));
 
 	// Verify that the factory can be instantiated
 	it('add new idea controller should be instantiable', function () {
 		expect(addNewIdeaTest).toBeDefined();
+	});
+	it('update idea details controller should be instantiable', function () {
+		expect(updateIdeaTest).toBeDefined();
 	});
 	it('idea details controller should be instantiable', function () {
 		expect(detailsIdeaTest).toBeDefined();
