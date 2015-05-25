634184777d428bb0554b72dcd1befe60696680bc
alxx88 <Aliaksei_Liubetski@epam.com>
2015-05-25 14:19:54 +0300

    js unit test
    Add unit test for controllers

---
+++ src/test/javascript/unit/controller.spec.js
@@ -0,0 +1,50 @@
+describe('Idea controllers testing', function(){
+
+	var scope;
+	var $location;
+
+	//controllers;
+	var addNewIdeaTest;
+	var updateIdeaTest;
+	var detailsIdeaTest;
+	var ideasCtrlTest;
+
+	beforeEach(module('app.controllers'));
+
+	beforeEach(inject(function($rootScope, $controller, _$location_) {
+		$location = _$location_;
+		scope = $rootScope.$new();
+
+		addNewIdeaTest = function(){
+				return $controller('addNewIdea', {
+					'$scope':scope
+				});
+		};
+
+		//detailsCtrl
+		detailsIdeaTest  = function(){
+				return $controller('detailsCtrl', {
+					'$scope':scope
+				});
+		};
+		//ideasCtrl
+		ideasCtrlTest  = function(){
+				return $controller('ideasCtrl', {
+					'$scope':scope
+				});
+		};
+	}));
+
+	// Verify that the factory can be instantiated
+	it('add new idea controller should be instantiable', function () {
+		expect(addNewIdeaTest).toBeDefined();
+	});
+	it('idea details controller should be instantiable', function () {
+		expect(detailsIdeaTest).toBeDefined();
+	});
+	it('idea details controller should be instantiable', function () {
+		expect(detailsIdeaTest).toBeDefined();
+	});
+
+});
+
