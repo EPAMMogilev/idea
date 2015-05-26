a2ca86b43af1c3f22ee5624fdd82bcde5a5d3b69
alxx88 <Aliaksei_Liubetski@epam.com>
2015-05-26 09:37:25 +0300

    update unit test

--- src/test/javascript/unit/controller.spec.js
+++ src/test/javascript/unit/controller.spec.js
@@ -1,7 +1,7 @@
-describe('Idea controllers testing', function(){
+﻿describe('Idea controllers testing', function(){
 
 	var scope;
-	var $location;
+	var state;
 
 	//controllers;
 	var addNewIdeaTest;
@@ -9,34 +9,61 @@ describe('Idea controllers testing', function(){
 	var detailsIdeaTest;
 	var ideasCtrlTest;
 
-	beforeEach(module('app.controllers'));
+	//scopes
+	var addNewIdeaScope;
+	var detailsIdeaScope;
+	var ideasCtrlIdeaScope;
+	var updateIdeaScope;
 
-	beforeEach(inject(function($rootScope, $controller, _$location_) {
-		$location = _$location_;
-		scope = $rootScope.$new();
+	//state
+	var state;
+
+	beforeEach(angular.mock.module('ui.router'));
+	beforeEach(angular.mock.module('app.controllers'));
+	beforeEach(angular.mock.module('app.services'));
+	beforeEach(angular.mock.module('ideaFactories'));
+	//beforeEach(angular.mock.module('ideaApp'));
+
+
+	beforeEach(inject(function($state, $rootScope, $controller) {
+		addNewIdeaScope = $rootScope.$new();
+		detailsIdeaScope = $rootScope.$new();
+		ideasCtrlIdeaScope = $rootScope.$new();
+		updateIdeaScope = $rootScope.$new();
+
+		state = $state;
+
+		//details
+		var vIdeaDetails = {
+				description: 'Some text'
+			};
 
 		addNewIdeaTest = function(){
 				return $controller('addNewIdea', {
-					'$scope':scope
+					$scope:addNewIdeaScope
 				});
 		};
 
 		//detailsCtrl
 		detailsIdeaTest  = function(){
 				return $controller('detailsCtrl', {
-					'$scope':scope
+					$scope:detailsIdeaScope,
+					$state:state,
+					ideaDetails: vIdeaDetails
 				});
 		};
 		//ideasCtrl
 		ideasCtrlTest  = function(){
 				return $controller('ideasCtrl', {
-					'$scope':scope
+					$scope:ideasCtrlIdeaScope
 				});
 		};
 		//updateIdea
 		updateIdeaTest = function(){
 				return $controller('updateIdea', {
-					'$scope':scope
+					$scope:updateIdeaScope,
+					$state:state,
+					ideaDetails: vIdeaDetails
 				});
 		};
 	}));
@@ -54,6 +81,51 @@ describe('Idea controllers testing', function(){
 	it('idea details controller should be instantiable', function () {
 		expect(detailsIdeaTest).toBeDefined();
 	});
+
+	//look at scope variable in controller	
+	it('add new idea controller check scope', function () {
+		// Make our assertions
+		expect(addNewIdeaScope).toBeDefined();
+		//addNewIdeaTest
+		var ctrl = addNewIdeaTest();
+		expect(ctrl).toBeDefined();
+		
+		expect(addNewIdeaScope.bottomButtonName).toBeDefined();
+		expect(addNewIdeaScope.bottomButtonName).toEqual('Добавить');
+		expect(addNewIdeaScope.data).toBeNull();
+	});
+
+	//look at scope variable in controller	
+	it('update new idea controller check scope', function () {
+		expect(updateIdeaScope).toBeDefined();
+
+		var ctrl = updateIdeaTest();
+		expect(ctrl).toBeDefined();
+
+		// Make our assertions		
+		expect(updateIdeaScope.bottomButtonName).toBeDefined();
+		expect(updateIdeaScope.bottomButtonName).toBe('Обновить');
+		expect(updateIdeaScope.data).toBeDefined();
+	});
+
+
+	//look at scope variable in controller	
+	it('idea details controller check scope', function () {
+		expect(detailsIdeaScope).toBeDefined();
+
+		var ctrl = detailsIdeaTest();
+		expect(ctrl).toBeDefined();
+		// Make our assertions
+		expect(ctrl.data).toBeDefined();
+	});
+
+	//look at scope variable in controller	
+	it('idea controller check scope', function () {
+
+		var ctrl = ideasCtrlTest();
+		expect(ctrl).toBeDefined();
+	});
+
 
 });
 
