0e2deda9e6e941a4b7be5e235fd7a9ad796b87f2
alxx88 <Aliaksei_Liubetski@epam.com>
2015-05-29 16:57:52 +0300

    update unit test

--- src/test/java/com/epam/idea/core/service/impl/CommentServiceImplTest.java
+++ src/test/java/com/epam/idea/core/service/impl/CommentServiceImplTest.java
@@ -1,22 +1,29 @@
 package com.epam.idea.core.service.impl;
 
 import com.epam.idea.builder.model.TestCommentBuilder;
+import com.epam.idea.builder.model.TestUserBuilder;
 import com.epam.idea.core.model.Comment;
+import com.epam.idea.core.model.User;
 import com.epam.idea.core.repository.CommentRepository;
 import com.epam.idea.core.service.CommentService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 /**
  * Created by Aliaksei_Liubetski on 2015-05-29.
@@ -36,6 +43,24 @@ public class CommentServiceImplTest {
     }//setUp
 
     @Test
+    public void shouldSaveOneComment(){
+        Comment comment = TestCommentBuilder.aComment().build();
+
+        given(this.commentRepositoryMock.findOne(eq(comment.getId()))).willReturn(Optional.of(comment));
+
+        this.commentService.save(comment);
+
+        ArgumentCaptor<Comment> userComment = ArgumentCaptor.forClass(Comment.class);
+        verify(this.commentRepositoryMock, times(1)).save(userComment.capture());
+        verifyNoMoreInteractions(this.commentRepositoryMock);
+
+        Comment commentArgument = userComment.getValue();
+        assertThat(commentArgument.getBody()).isEqualTo(comment.getBody());
+        assertThat(commentArgument.getSubject()).isEqualTo(comment.getSubject());
+        assertThat(commentArgument.getId()).isEqualTo(comment.getId());
+    }//shouldAddNewComment
+
+    @Test
     public void shouldFindOneComment(){
         Comment comment = TestCommentBuilder.aComment().build();
 
@@ -45,4 +70,13 @@ public class CommentServiceImplTest {
 
         assertThat(actual).isEqualTo(comment);
     }//shouldAddNewComment
+
+    @Test
+    public  void souldfindCommentsByUserIdComment(){
+        User userToFind = TestUserBuilder.aUser().build();
+
+        //findCommentsByUserId
+        List<Comment> comments = this.commentService.findCommentsByUserId(userToFind.getId());
+        assertThat(comments).isNotNull();
+    }//souldfindCommentsByUserIdComment
 }
