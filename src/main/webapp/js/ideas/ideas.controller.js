d63984bb494cead9b35ab06ed7c9a6179c9b5eb2
alxx88 <Aliaksei_Liubetski@epam.com>
2015-06-09 14:20:47 +0300

    add points 2 main screen

--- src/main/webapp/js/ideas/ideas.controller.js
+++ src/main/webapp/js/ideas/ideas.controller.js
@@ -4,25 +4,58 @@
         .module('app.controllers')
         .controller('ideasCtrl', ideasCtrl);
 
-    ideasCtrl.$inject = ['$scope', 'ideasFactory', 'tagsFactory'];
+    ideasCtrl.$inject = ['$scope', 'ideasFactory', 'tagsFactory', 'mapGeoService'];
 
-    function ideasCtrl($scope, ideasFactory, tagsFactory) {
+    function ideasCtrl($scope, ideasFactory, tagsFactory, mapGeoService) {
 
         var vm = this;
+        $scope.geoObjects = null;// = new Array();
+        /*
+        $scope.updateGeoObjects = function(ideas){
+            if(ideas){
+                for(var i=0; i<ideas.length; i++){
+                    var oldObject = ideas[i];
 
+                    if(oldObject && oldObject.longitude && oldObject.latitude){
+                        var newGeoObject = {
+                            geometry: {
+                                        type: 'Point',
+                                        coordinates: [oldObject.longitude,oldObject.latitude]
+                                    },
+                            properties: {
+                                balloonContent: oldObject.title,
+                                clusterCaption: 'идея #' + oldObject.id
+                            }
+                        };
+                        //add 2 array
+                        $scope.geoObjects.push(newGeoObject);
+                    }//if
+                }//for
+            }//if
+        };//updateGeoObjects
+        */
         vm.selectByCategory =function (tag) {
           tagsFactory.getIdeasByTag(tag).then(function (ideas) {
             vm.ideas = ideas;
+
+            //$scope.updateGeoObjects(ideas);
+            $scope.geoObjects = mapGeoService.generateGeoObjects(ideas);
         })
         };
 
         ideasFactory.getIdeas().then(function (ideas) {
             vm.ideas = ideas;
+
+            //$scope.updateGeoObjects(ideas);
+            $scope.geoObjects = mapGeoService.generateGeoObjects(ideas);
         });
 
         $scope.$on('ideas-update', function() {
             ideasFactory.getIdeas().then(function (ideas) {
                 vm.ideas = ideas;
+
+            //$scope.updateGeoObjects(ideas);
+            $scope.geoObjects = mapGeoService.generateGeoObjects(ideas);
             });
         });
 
@@ -33,6 +66,14 @@
 
 			console.log('Go to Details');
 			$state.go('ideaDetails', { 'idea': angular.toJson(ideaDetail) });
+        };
+
+        //gui effects
+        $scope.mouseenter=function(e){
+            e.get('target').options.set('preset', 'islands#greenIcon');
+        };
+        $scope.mouseleave=function(e){
+            e.get('target').options.unset('preset');
         };
     }
 
