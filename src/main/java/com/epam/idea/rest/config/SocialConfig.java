cd8ed2fe30c40fc5cb4f5d4e23b03c30f7843371
Ihar_Niakhlebau <Ihar_Niakhlebau@epam.com>
2015-06-18 11:59:44 +0300

    Add facebook and vk authentication Enable csrf protect

--- src/main/java/com/epam/idea/rest/config/SocialConfig.java
+++ src/main/java/com/epam/idea/rest/config/SocialConfig.java
@@ -17,8 +17,10 @@ import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
+import org.springframework.social.vkontakte.connect.VKontakteConnectionFactory;
 
 /**
  * Created by Ihar_Niakhlebau on 04-Jun-15.
@@ -29,6 +31,10 @@ public class SocialConfig implements SocialConfigurer {
 
 	public static String googleClientId = "312001620105-udtemaddk1ii34rqq0cs07lso3co1gbl.apps.googleusercontent.com";
 	public static String googleClientSecret = "CTK9wEzU7VePQS7PhNcbwd3E";
+	public static String facebookClientId = "1461506590828890";
+	public static String facebookClientSecret = "1dc9045cb99ae97b3686cb10648b29b8";
+	public static String vkClientId = "4960267";
+	public static String vkClientSecret = "uu1iDGVhSi8h6EGsL0Gm";
 
 	@Autowired
 	private DataSource dataSource;
@@ -38,6 +44,12 @@ public class SocialConfig implements SocialConfigurer {
 		GoogleConnectionFactory googleConnectionFactory = new GoogleConnectionFactory(googleClientId, googleClientSecret);
 		googleConnectionFactory.setScope("https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email");
 		cfConfig.addConnectionFactory(googleConnectionFactory);
+		FacebookConnectionFactory facebookConnectionFactory = new FacebookConnectionFactory(facebookClientId, facebookClientSecret);
+		facebookConnectionFactory.setScope("email");
+		cfConfig.addConnectionFactory(facebookConnectionFactory);
+		VKontakteConnectionFactory vKontakteConnectionFactory = new VKontakteConnectionFactory(vkClientId, vkClientSecret);
+		vKontakteConnectionFactory.setScope("email");
+		cfConfig.addConnectionFactory(vKontakteConnectionFactory);
 
 	}
 
