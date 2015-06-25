7337c303509a1fb7b697c11f064a2e89f3efbbaf
alxx88 <Aliaksei_Liubetski@epam.com>
2015-06-25 13:00:28 +0300

    cahnge directive

--- src/main/webapp/js/ideas/ideasList.directive.js
+++ src/main/webapp/js/ideas/ideasList.directive.js
@@ -11,12 +11,20 @@
 		return {
 			restrict: 'A',
 			scope:{
-			    updateFunction: '&'
+			    updateFunction: '&',
+			    divName: '@'
 			},
 			link: function(scope, element, attrs) {
                 element.bind('scroll', function(){
-                    alert('scroll: ');
-                    scope.updateFunction();
+                    var scrollTop = element.scrollTop();
+                    var scrollH = $(scope.divName).height();
+
+                    var scrollPercent = 100 - Math.round(scrollTop / (scrollH/100));
+
+                    if(scrollPercent > 99){
+                        alert('% = ' + scrollPercent);
+                        scope.updateFunction();
+                    }//if
                 });
 			}//link
 		}
