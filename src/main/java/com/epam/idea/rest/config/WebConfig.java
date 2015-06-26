a224fa3611fa68bbb59a6df877e2d084fe253eb4
alxx88 <Aliaksei_Liubetski@epam.com>
2015-06-26 15:21:09 +0300

    fix test bug

---
+++ src/main/java/com/epam/idea/rest/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.epam.idea.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * Created by Katsiaryna_Mikhalcha on 6/24/2015.
+ */
+
+@Configuration
+@EnableWebMvc
+@EnableSpringDataWebSupport
+public class WebConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+	}
+}
