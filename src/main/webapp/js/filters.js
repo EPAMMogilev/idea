52f162b4d6257418fa34f01f149a34e999f9ee8d
alxx88 <Aliaksei_Liubetski@epam.com>
2015-06-30 15:51:39 +0300

    add filter by tag (tag will start # or @)

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -34,12 +34,33 @@ angular
                 filtered = items.slice();
             }//if
         }else{
-            for(var i=0; i<items.length; i++){
-                var item = items[i];
-                if(item.title.indexOf(criteria)>=0 || item.description.indexOf(criteria)>=0){
-                    filtered.push(item);
-                }//if
-            }//for
+            //if first symbol # or @ - then this is tag filter, else - fulltext filter
+            if(criteria.charAt(0) == '@' || criteria.charAt(0) == '#'){
+                var tagData = criteria.substring(1);
+                if(tagData.length>0){
+                    for(var i=0; i<items.length; i++){
+                        var item = items[i];
+                        if(item.tags){
+                            for(var j=0; j<item.tags.length; j++){
+                                var tag = item.tags[j];
+                                if(tag.name.indexOf(tagData)>=0){
+                                    filtered.push(item);
+                                    break;
+                                }//if
+                            }//for
+                        }//if
+                    }//for
+                }else{
+                    filtered = items.slice();
+                }//if..else..
+            }else{
+                for(var i=0; i<items.length; i++){
+                    var item = items[i];
+                    if(item.title.indexOf(criteria)>=0 || item.description.indexOf(criteria)>=0){
+                        filtered.push(item);
+                    }//if
+                }//for
+            }//if..else..
         }//if..else..
 
         return filtered;
