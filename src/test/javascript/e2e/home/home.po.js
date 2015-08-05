26a188cbed158fb1821208b45c298f6b2c5a201d
Katsiaryna Mikhalchanka <katsiaryna_mikhalchanka@epam.com>
2015-08-05 11:55:06 +0300

    Add e2e tests for "like" feature

--- src/test/javascript/e2e/home/home.po.js
+++ src/test/javascript/e2e/home/home.po.js
@@ -5,13 +5,18 @@ var HomePage = function() {
 HomePage.prototype = Object.create({}, {
 	popularIdeas: {get: function() { return element.all(by.repeater('idea in ideasCtrl.popular')); }},
 	latestIdeas: {get: function() { return element.all(by.repeater('idea in ideasCtrl.latest')); }},
-	tagButtons: {get: function() { return element.all(by.repeater('tag in tagsCtrl.tagsTop')); }},
 	allButton: {get: function() { return element(by.id('all')); }},
+	tagButtons: {get: function() { return element.all(by.repeater('tag in tagsCtrl.tagsTop')); }},
+	likeButtons: {get: function() {return element.all(by.css('.btn-thumbs')); }},
+	ratings: {get: function() { return element.all(by.id('rating')); }},
 	loginButton: {get: function() { return element(by.id('words')); }},
+
 
 	getByTag: {value: function(id) {return this.tagButtons.get(id).click(); }},
 	getAll: {value: function() { return this.allButton.click(); }},
-	login: {value: function() { return this.loginButton.click(); }}
+	login: {value: function() { return this.loginButton.click(); }},
+	like: {value: function(id) { return this.likeButtons.get(id).click(); }},
+	getRating: {value: function(id) { return this.ratings.get(id).getText(); }}
 });
 
 module.exports = HomePage;
--- src/test/javascript/e2e/home/home.spec.js
+++ src/test/javascript/e2e/home/home.spec.js
@@ -1,14 +1,21 @@
 'use strict';
 
 var HomePage = require('../home/home.po.js');
+var LoginPage = require('../login/login.po.js');
 
 describe('home page test', function() {
 
 	var homePage;
+	var loginPage;
 
 	beforeEach(function() {
 		homePage = new HomePage();
 	});
+
+	function login() {
+		loginPage = new LoginPage();
+		loginPage.login('first@idea.com', '1234');
+	};
 
 	it('should filter results ideas list after press button - all', function() {
 		homePage.getAll();
@@ -17,28 +24,40 @@ describe('home page test', function() {
 	});
 
 	it('should filter results ideas list after press 1st tag button', function() {
-    	homePage.getByTag(0);
-    	expect(homePage.popularIdeas.count()).toEqual(0);
-    	expect(homePage.latestIdeas.count()).toEqual(0);
-    });
+		homePage.getByTag(0);
+		expect(homePage.popularIdeas.count()).toEqual(0);
+		expect(homePage.latestIdeas.count()).toEqual(0);
+	});
 
-    it('should filter results ideas list after press 2nd tag button', function() {
-    	homePage.getByTag(1);
-    	expect(homePage.popularIdeas.count()).toEqual(3);
-    	expect(homePage.latestIdeas.count()).toEqual(3);
-    });
+	it('should filter results ideas list after press 2nd tag button', function() {
+		homePage.getByTag(1);
+		expect(homePage.popularIdeas.count()).toEqual(3);
+		expect(homePage.latestIdeas.count()).toEqual(3);
+	});
 
-    it('should filter results ideas list after press 3rd tag button', function() {
-    	homePage.getByTag(2);
-    	expect(homePage.popularIdeas.count()).toEqual(2);
-    	expect(homePage.latestIdeas.count()).toEqual(2);
-    });
+	it('should filter results ideas list after press 3rd tag button', function() {
+		homePage.getByTag(2);
+		expect(homePage.popularIdeas.count()).toEqual(2);
+		expect(homePage.latestIdeas.count()).toEqual(2);
+	});
 
-    it('should open login page after press login button', function() {
-        homePage.login();
+	it('should open login page after press login button', function() {
+		homePage.login();
 
-        browser.getLocationAbsUrl().then(function(url) {
-        	expect(url.split('%')[0].split('#')[1]).toBe('/login');
-        });
-    });
+		browser.getLocationAbsUrl().then(function(url) {
+			expect(url.split('%')[0].split('#')[1]).toBe('/login');
+		});
+	});
+
+	it('should increment idea rating', function() {
+		login();
+		homePage.like(0);
+		expect(homePage.getRating(0)).toBe('32');
+	});
+
+	it('should decrement idea rating', function() {
+		login();
+		homePage.like(0);
+		expect(homePage.getRating(0)).toBe('31');
+	});
 });
