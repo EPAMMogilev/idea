b9ddc002bdd872c4cf6130091654de779b6d2028
SviatlanaHlukhava <Sviatlana_Hlukhava@epam.com>
2015-08-13 12:38:23 +0000

    Add unit tests

--- src/test/javascript/unit/services.spec.js
+++ src/test/javascript/unit/services.spec.js
@@ -16,10 +16,24 @@ describe("app.services module", function() {
      });
    });
 
-   describe('authenticationService', function () {
+   describe('authorizationService', function () {
        var $httpBackend, location;
 
        var testAuthorizationService;
+
+	   var role = {
+               id: 1,
+               authority: "ROLE_USER"
+    	   };
+	   var user = {
+               id: 1,
+               userName: "firstUser",
+               authorities: [role]
+    	   };
+	   var idea = {
+           id: 1,
+           author: user
+	   };
 
 	    beforeEach(function() {
 	    	module('ideaFactories');
@@ -29,6 +43,7 @@ describe("app.services module", function() {
     	   $httpBackend = _$httpBackend_;
            testAuthorizationService = authorizationService;
            location = $location;
+           spyOn(location, 'path');
        }));
 
 
@@ -37,21 +52,41 @@ describe("app.services module", function() {
            $httpBackend.verifyNoOutstandingRequest();
        });
 
-       it('check redirect from Edit page to Access Denied if user is not an author or admin', function () {
-    	   var user = {
-                   id: 1,
-                   userName: "firstUser"
+       it('check redirect from Edit page to Access Denied if user is not an author or admin', inject(function(_$rootScope_) {
+    	   var currUser = {
+                   id: 2,
+                   userName: "secondUser",
+                   authorities: [role]
         	   };
-    	   var idea = {
-               id: 1,
-               author: user
-    	   };
-           spyOn(location, 'path');
+    	   _$rootScope_.currentUser = currUser;
            $httpBackend.expectGET('api/v1/ideas/1').respond(idea);
            testAuthorizationService.redirectFromEditToAccessDeniedIfNeeded(idea);
            $httpBackend.flush();
            expect(location.path).toHaveBeenCalledWith('/accessDenied');
-       });
+       }));
+
+       it('check no redirect from Edit page to Access Denied if user is an author', inject(function(_$rootScope_) {
+    	   _$rootScope_.currentUser = user;
+           $httpBackend.expectGET('api/v1/ideas/1').respond(idea);
+           testAuthorizationService.redirectFromEditToAccessDeniedIfNeeded(idea);
+           $httpBackend.flush();
+           expect(location.path).not.toHaveBeenCalledWith('/accessDenied');
+       }));
+
+       it('check no redirect from Edit page to Access Denied if user is admin', inject(function(_$rootScope_) {
+    	   var roleAdmin = {
+                   id: 1,
+                   authority: "ROLE_ADMIN"
+        	   };
+    	   var currUser = {
+                   id: 3,
+                   userName: "admin",
+                   authorities: [roleAdmin]
+        	   };
+    	   _$rootScope_.currentUser = currUser;
+           testAuthorizationService.redirectFromEditToAccessDeniedIfNeeded(idea);
+           expect(location.path).not.toHaveBeenCalledWith('/accessDenied');
+       }));
     });
 
 	/*beforeEach(inject(function(detailsService) {
